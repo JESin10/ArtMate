16b06960a064be511ad76462a6786880477d2b83
JESin10 <shinej1029@gmail.com>
2023-09-13 18:28:04 +0900

    [feat] : Initial Setting

---
+++ src/page/NearBy.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NearBy() {
+  return <div>NearBy</div>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkmode: "class",
+  // main color setting 가능
+  theme: {
+    screens: {},
+    extend: {
+      colors: {
+        primary: {
+          YellowGreen: "#608D00",
+          Gray: "#8D8D8D",
+        },
+        secondary: {},
+      },
+      spacing: {},
+    },
+    fontFamily: {
+      NotoSansCJK: ["NotoSansCJK KR"],
+    },
+  },
+  variants: {
+    extend: {
+      borderWidth: ["hover"],
+    },
+  },
+};
