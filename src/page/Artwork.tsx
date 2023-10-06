56d44214820737f0c642261303a5e31802f9245e
JESin10 <shinej1029@gmail.com>
2023-10-06 17:02:40 +0900

    [feat] : Artwork Page Edit

--- src/page/Artwork.tsx
+++ src/page/Artwork.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useQuery } from "react-query";
-import { SeoulArtMuseum_OpenData } from "../api/Gallery_OpenApi";
+import { SeoulArtMuseum_ArtWork_OpenData } from "../api/Gallery_OpenApi";
 import Search_Bar from "../component/Search_Bar";
 import tw from "tailwind-styled-components";
 import { loadImg } from "../assets/images";
@@ -28,10 +28,10 @@ export interface ArtworkInfo {
 }
 
 export default function Artwork() {
-  const [artworklist, setArtWorkList] = useState([]);
+  const [artworkList, setArtWorkList] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-  const { data } = useQuery("DP_EX_NO", SeoulArtMuseum_OpenData, {
+  const { data } = useQuery("DP_EX_NO", SeoulArtMuseum_ArtWork_OpenData, {
     onSuccess: (data) => {
       setArtWorkList(data.ListExhibitionOfSeoulMOAInfo.row);
     },
@@ -49,52 +49,57 @@ export default function Artwork() {
     <>
       <Search_Bar />
       <div className="w-full h-screen flex flex-col items-center bg-black/30 overflow-y-auto pb-[120px]">
-        <div className="w-[350px] h-fit my-4 mx-auto">
+        {/* <div className="w-[350px] h-fit my-4 mx-auto">
           <input
             placeholder="장소, 주소를 검색해보세요"
             className="w-full h-[35px] indent-4 text-sm border-black border-2 rounded-3xl"
           />
-        </div>
+        </div> */}
         <div className="w-full h-fit">
           <div className="w-11/12 mx-auto ">
             <div className="flex justify-between">
               <h1 className="text-3xl font-extrabold my-2">작품 정보</h1>
               <div className="flex space-x-1">
                 <button>
-                  <img src={loadImg.Map_current1} />
+                  <img src={"./icons/Outline/filter.svg"} />
                 </button>
                 <button>
-                  <img src={loadImg.Map_all} />
+                  <img src={"./icons/Outline/reload.svg"} />
                 </button>
               </div>
             </div>
-            {artworklist.length > 0 &&
-              artworklist.map((list: ArtworkInfo, index: number) => (
-                <div className="flex flex-col space-y-2">
-                  <GalleryContainer onClick={openModal}>
-                    <div className="w-36 h-36 my-2">
-                      <img
-                        className="w-full h-full object-cover rounded-xl mx-2 justify-center shadow-Ver1"
-                        alt="gallery"
-                        src={list.DP_MAIN_IMG}
-                      />
-                    </div>
-                    <div className="flex flex-col h-fit my-auto justify-center px-2">
-                      <div className="w-40 h-[22px] bg-white font-extrabold text-base overflow-hidden text-ellipsis break-all line-clamp-1 flex-wrap my-1">
-                        {list.DP_NAME}
+            <div className="flex flex-wrap justify-center">
+              {artworkList.length > 0 &&
+                artworkList.map((list: ArtworkInfo, index: number) => (
+                  <div className="flex flex-col w-fit justify-center mx-auto">
+                    <GalleryContainer onClick={openModal}>
+                      <div className="w-36 h-32 my-3">
+                        <img
+                          className="w-full h-full object-cover rounded-xl justify-center shadow-Ver1"
+                          alt="gallery"
+                          src={list.DP_MAIN_IMG}
+                        />
                       </div>
-                      <div className="text-sm">{list.DP_PLACE}</div>
-                      <div className="text-sm">휴관일</div>
-                      <div className="text-right text-sm">00m</div>
-                    </div>
-                  </GalleryContainer>
-                  <Artwork_Modal
-                    isOpen={isModalOpen}
-                    closeModal={closeModal}
-                    artworkInfo={list}
-                  />
-                </div>
-              ))}
+                      <div className="w-36 h-fit flex flex-col my-auto justify-center">
+                        <div className="h-[22px] my-2 bg-white font-extrabold text-base overflow-hidden text-ellipsis break-all line-clamp-1 flex-wrap my-1">
+                          {list.DP_NAME}
+                        </div>
+                        <div className="text-xs overflow-hidden text-ellipsis break-all line-clamp-1 flex-wrap">
+                          {list.DP_ARTIST}
+                        </div>
+                        <div className="text-xs overflow-hidden text-ellipsis break-all line-clamp-1 flex-wrap">
+                          {list.DP_ART_PART}
+                        </div>
+                      </div>
+                    </GalleryContainer>
+                    <Artwork_Modal
+                      isOpen={isModalOpen}
+                      closeModal={closeModal}
+                      artworkInfo={list}
+                    />
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       </div>
@@ -103,8 +108,8 @@ export default function Artwork() {
 }
 
 const GalleryContainer = tw.div`
-  flex h-fit w-full justify-between
+  flex h-fit w-fit justify-between
   cursor-pointer rounded-lg
-  space-x-3
+  space-y-1 flex-col
   hover:bg-primary-YellowGreen/10
   `;
