24a2085475517accc19f27a08d133b4b25746216
JESin10 <shinej1029@gmail.com>
2023-10-06 17:26:08 +0900

    [feat] : Search_Bar Page Edit

--- src/component/Search_Bar.tsx
+++ src/component/Search_Bar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { loadImg } from "../assets/images";
 
 export default function Search_Bar() {
@@ -6,25 +6,63 @@ export default function Search_Bar() {
     fill: "#608D00",
   };
 
+  const [isInputVisible, setInputVisible] = useState<boolean>(false);
+
+  const handleSearchClick = () => {
+    setInputVisible((prevVisible) => !prevVisible);
+  };
+
   return (
     <div className="py-4 bg-red-100 flex-col mx-auto justify-center ">
-      <img
-        className="w-36 h-fit"
-        alt="main-logo"
-        style={svgStyle}
-        // style={{ filter: "brightness(0) invert(1) hue-rotate(120deg)" }}
-        src={loadImg.Main_TextLogo}
-      />
-      <div className="flex">
-        <input type="text" className="border-primary-YellowGreen border-2" />
-        <button onClick={() => window.alert("준비중입니다")}>
-          <img
-            className="w-10 h-10"
-            src={"./icons/Outline/search.svg"}
-            alt="Search"
-            style={{ fill: "#608D00" }}
-          />
-        </button>
+      <div className="flex flex-col">
+        {isInputVisible ? (
+          <>
+            <div className="flex border-primary-YellowGreen border-b-2 py-2 w-4/5 justify-center mx-auto">
+              <img
+                className="w-30 h-fit mx-auto"
+                alt="main-logo"
+                style={svgStyle}
+                // style={{ filter: "brightness(0) invert(1) hue-rotate(120deg)" }}
+                src={loadImg.Main_TextLogo}
+              />
+            </div>
+            <div className="flex justify-between border-primary-YellowGreen border-2 bg-white w-4/5 rounded-xl mt-4 mx-auto">
+              <input
+                type="text"
+                className="w-full h-fit rounded-l-xl bg-transparent indent-0 px-3"
+              />
+              <button onClick={handleSearchClick}>
+                <img
+                  className="w-6 h-6 "
+                  src={"./icons/Outline/search.svg"}
+                  alt="Search"
+                  style={{ fill: "#608D00" }}
+                />
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="flex border-primary-YellowGreen border-b-2 py-2 w-4/5 justify-center mx-auto">
+            <img
+              className="w-30 h-fit mx-auto pl-6"
+              alt="main-logo"
+              style={svgStyle}
+              // style={{ filter: "brightness(0) invert(1) hue-rotate(120deg)" }}
+              src={loadImg.Main_TextLogo}
+            />
+            <button
+              onClick={handleSearchClick}
+              className="bg-transparent flex justify-end"
+            >
+              <img
+                className="w-6 h-6"
+                src={"./icons/Outline/search.svg"}
+                alt="Search"
+                style={{ fill: "#608D00" }}
+              />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
