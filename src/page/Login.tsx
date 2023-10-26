ef4c369865717b06ace460fa30924d0e5497a349
JESin10 <shinej1029@gmail.com>
2023-10-26 23:17:04 +0900

    [feat] : svg component setting

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { loadImg } from "../assets/images";
 import { useAuth } from "../modules/UserAuth";
 import { useNavigate } from "react-router-dom";
+import tw from "tailwind-styled-components";
+import { ReactComponent as MainLogo } from "../assets/CustomSvg/main_text_logo.svg";
+import { ReactComponent as DescLogo } from "../assets/CustomSvg/main_logo_desc.svg";
 
 export default function Login() {
   const { signup, signupWithGoogle, currentUser } = useAuth();
@@ -17,56 +20,86 @@ export default function Login() {
   };
 
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
-      <img
-        className="h-[64px]"
-        src={loadImg.Main_TextLogo}
-        style={{ fill: "#608D00" }}
-      />
-      <img className="h-[34px]" src={"./icons/Outline/main_logo_desc.svg"} />
+    <div className="w-full h-screen flex flex-col mt-20 justify-center items-center">
+      <div className="flex-col w-fit mx-auto justify-center mb-20">
+        <MainTextLogo />
+        <DescTextLogo />
+      </div>
       <div className="flex-col flex justify-center w-fit">
         <div className="w-fit my-5 flex flex-col space-y-4">
-          <input
-            type="text"
-            className="border-primary-YellowGreen border-2 rounded-3xl px-2 w-[320px] h-[42px]"
-            placeholder="아이디"
-          />
-          <input
-            type="text"
-            className="border-primary-YellowGreen border-2 rounded-3xl px-2 w-[320px] h-[42px]"
-            placeholder="비밀번호"
-          />
-          <div className="flex space-x-2 items-center w-fit">
-            <input type="radio" className="w-[18px] h-[18px]" />
+          <AuthInput type="text" placeholder="아이디" />
+          <AuthInput type="text" placeholder="비밀번호" />
+          <div className="flex space-x-2 items-center w-fit mx-2">
+            <input type="checkbox" className="w-4 h-4" />
             <p className="w-fit ">자동로그인</p>
           </div>
           <button className="bg-primary-YellowGreen rounded-3xl text-white w-[320px] h-[42px]">
             로그인
           </button>
         </div>
-        <div className="flex justify-center w-fit mx-auto">
-          <button className="bg-blue-200 mx-2">아이디 찾기</button>
-          <button className="bg-blue-300 mx-2">비밀번호 찾기</button>
-          <button className="bg-blue-400 mx-2">회원가입</button>
+        <div className="flex justify-center w-fit mx-auto text-xs mb-4">
+          <button className="mx-4">아이디 찾기</button>
+          <button className="mx-4">비밀번호 찾기</button>
+          <button className="mx-4">회원가입</button>
         </div>
-        <div className="my-4 flex flex-col justify-center items-center">
-          <p className="w-fit my-4">소셜로 시작하기</p>
-          <div className="flex flex-col space-y-4">
-            <button className="bg-yellow-400 rounded-3xl w-[320px] h-[42px]">
-              카카오 로그인
-            </button>
-            <button
+        <div className="my-10 flex flex-col justify-center items-center border-t-2 border-primary-Gray/60">
+          <p className="w-fit my-4 text-sm ">소셜로 시작하기</p>
+          <div className="flex flex-col space-y-4 font-semibold">
+            <SocialAuthBtn className="bg-[#FEE500]">
+              <SocialAuthBtnContainer>
+                <img alt="SocialAuth_Kakao" src={loadImg.Kakao_LoginBtn} />
+                <p>카카오로 시작하기</p>
+              </SocialAuthBtnContainer>
+            </SocialAuthBtn>
+            <SocialAuthBtn
               onClick={SignupWithGoogleHandler}
-              className="bg-white border-black border-2 rounded-3xl w-[320px] h-[42px]"
+              className=" bg-white border-black border-2"
             >
-              구글 로그인
-            </button>
-            <button className="bg-green-600 rounded-3xl w-[320px] h-[42px]">
-              네이버 로그인
-            </button>
+              <SocialAuthBtnContainer>
+                <img alt="SocialAuth_Google" src={loadImg.Google_LoginBtn} />
+                <p> 구글로 시작하기</p>
+              </SocialAuthBtnContainer>
+            </SocialAuthBtn>
+            <SocialAuthBtn className="bg-[#03C75A] ">
+              <SocialAuthBtnContainer>
+                <img
+                  alt="SocialAuth_Naver"
+                  className="w-10"
+                  src={loadImg.Naver_LoginBtn}
+                />
+                <p>네이버로 시작하기</p>
+              </SocialAuthBtnContainer>
+            </SocialAuthBtn>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+const MainTextLogo = tw(MainLogo)`
+fill-primary-YellowGreen
+h-[64px] w-[250px]
+`;
+
+const DescTextLogo = tw(DescLogo)`
+fill-primary-Gray
+h-[34px] w-[200px] mx-auto
+`;
+
+const AuthInput = tw.input`
+w-[320px] h-[42px] 
+border-primary-YellowGreen border-2 
+rounded-3xl px-2 text-center
+
+`;
+
+const SocialAuthBtn = tw.button`
+flex rounded-3xl items-center
+w-[320px] h-[42px] pr-4
+`;
+
+const SocialAuthBtnContainer = tw.div`
+flex w-fit justify-end items-center
+mx-auto
+`;
